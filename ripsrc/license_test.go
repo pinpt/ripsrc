6bc17f1fa099e8034c74984729c09a8363a53e2d
Jeff Haynie <jhaynie@pinpt.com>
2018-09-21 11:40:11 -0500

    expand filenames to search for licenses and add additional unit tests

--- ripsrc/license_test.go
+++ ripsrc/license_test.go
@@ -1,6 +1,7 @@
 package ripsrc
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,4 +35,57 @@ func main() {
 }`))
 	assert.NoError(err)
 	assert.Nil(lic)
+}
+
+func TestLicenseMITFilename(t *testing.T) {
+	assert := assert.New(t)
+	lic, err := detect("LICENSE-MIT", []byte(`Copyright 2018 Pinpoint
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`))
+	assert.NoError(err)
+	assert.NotNil(lic)
+	assert.Equal("MIT", lic.Name)
+	assert.Equal(float32(0.9814815), lic.Confidence)
+}
+
+func TestLicenseMITFilenameInDirectory(t *testing.T) {
+	assert := assert.New(t)
+	lic, err := detect("this/is/some/directory/license.txt", []byte(`Copyright 2018 Pinpoint
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`))
+	assert.NoError(err)
+	assert.NotNil(lic)
+	assert.Equal("MIT", lic.Name)
+	assert.Equal(float32(0.9814815), lic.Confidence)
+}
+
+func TestLicenseConcurrency(t *testing.T) {
+	assert := assert.New(t)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			lic, err := detect("this/is/some/directory/license.txt", []byte(`Copyright 2018 Pinpoint
+
+			Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+			The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+			THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`))
+			assert.NoError(err)
+			assert.NotNil(lic)
+			assert.Equal("MIT", lic.Name)
+			assert.Equal(float32(0.9814815), lic.Confidence)
+		}()
+	}
+	wg.Wait()
 }
