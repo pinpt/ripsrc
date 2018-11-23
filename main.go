9353fc9d4dd8b977adafd16ec70b4ee85811bdb8
Jeff Haynie <jhaynie@pinpt.com>
2018-11-23 13:20:04 -0600

    removed old commented out code

--- main.go
+++ main.go
@@ -3,12 +3,5 @@ package main
 import "github.com/pinpt/ripsrc/cmd"
 
 func main() {
-	// d := diffmatchpatch.New()
-	// o, _ := ioutil.ReadFile("out")
-	// p, err := d.PatchFromText(string(o))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(p)
 	cmd.Execute()
 }
