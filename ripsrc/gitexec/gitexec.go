519143cad251b221e87ad36f80e96caf9462ea7f
Andris Valums <andris.valums@pinpt.com>
2019-01-08 01:22:41 +0100

    BE-1089 compress git log output

--- ripsrc/gitexec/gitexec.go
+++ ripsrc/gitexec/gitexec.go
@@ -2,6 +2,7 @@ package gitexec
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
@@ -17,17 +18,44 @@ import (
 
 const cacheDir = ".pp-git-cache"
 
+type fileCloser struct {
+	f *os.File
+	io.ReadCloser
+}
+
+func (f fileCloser) Close() error {
+	err := f.ReadCloser.Close()
+	if err != nil {
+		return err
+	}
+	return f.f.Close()
+}
+
+func newGzipFileCloser(f *os.File) (io.ReadCloser, error) {
+	gr, err := gzip.NewReader(f)
+	if err != nil {
+		return nil, err
+	}
+
+	res := fileCloser{}
+	res.f = f
+	res.ReadCloser = gr
+	return res, nil
+}
+
+const casheVersion = "1"
+
 func ExecWithCache(ctx context.Context, gitCommand string, repoDir string, args []string) (io.ReadCloser, error) {
 	start := time.Now()
 	headCommit := headCommit(ctx, gitCommand, repoDir)
-	cacheKey := hashString(strings.Join(args, "@") + headCommit)
+	cacheKey := hashString(casheVersion + "@" + strings.Join(args, "@") + headCommit)
 
-	loc := filepath.Join(repoDir, cacheDir, cacheKey+".txt")
+	loc := filepath.Join(repoDir, cacheDir, cacheKey+".txt.gz")
 
 	f, err := os.Open(loc)
 	if err == nil {
 		fmt.Println("Using cache for ", repoDir, strings.Join(args, " "))
-		return f, nil
+		return newGzipFileCloser(f)
 	} else {
 		if !os.IsNotExist(err) {
 			panic(fmt.Errorf("could not open file at location %v, err %v", loc, err))
@@ -36,18 +64,10 @@ func ExecWithCache(ctx context.Context, gitCommand string, repoDir string, args 
 
 	os.MkdirAll(path.Dir(loc), 0777)
 
-	f, err = os.Create(loc + ".tmp")
-	defer f.Close()
+	err = execToFile(ctx, loc+".tmp", gitCommand, repoDir, args)
 	if err != nil {
 		return nil, err
 	}
-
-	err = ExecIntoWriter(ctx, f, gitCommand, repoDir, args)
-	if err != nil {
-		return nil, err
-	}
-
-	f.Close()
 
 	err = os.Rename(loc+".tmp", loc)
 	if err != nil {
@@ -56,8 +76,30 @@ func ExecWithCache(ctx context.Context, gitCommand string, repoDir string, args 
 
 	fmt.Println("Took", time.Since(start), "to run git", repoDir, strings.Join(args, " "))
 
-	return os.Open(loc)
+	f, err = os.Open(loc)
+	if err != nil {
+		return nil, err
+	}
+	return newGzipFileCloser(f)
 
+}
+
+func execToFile(ctx context.Context, loc string, gitCommand string, repoDir string, args []string) error {
+	f, err := os.Create(loc)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	gw := gzip.NewWriter(f)
+	defer gw.Close()
+
+	err = ExecIntoWriter(ctx, gw, gitCommand, repoDir, args)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func hashString(str string) string {
