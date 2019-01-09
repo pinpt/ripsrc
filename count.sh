89779b129b8757d8b1dc64262690008eb659d1db
J Ordaz <jordaz@pinpt.com>
2019-01-08 18:48:33 -0600

    Removed temp files

--- count.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CommandResult="Interface    Chipset     Driver     mon0    Unknown      iwlwifi - [phy0]wlan0       Unknown     iwlwifi - [phy0]"
-InstanceId="mon0";
-count=`grep -o "$InstanceId" <<< "$CommandResult" | wc -l`
-echo "$InstanceId encountered "$count" times";
