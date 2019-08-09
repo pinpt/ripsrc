82041d7f66950877244c79ee252c70c1b0ba74ee
xzandrew <andris.valums@pinpt.com>
2019-08-09 17:00:22 +0200

    Revert "APP-1751 Additional git log filtering in incremental runs"

--- ripsrc/commit_info.go
+++ ripsrc/commit_info.go
@@ -10,7 +10,6 @@ func (s *Ripsrc) getCommitInfo(ctx context.Context) error {
 	copts := commitmeta.Opts{}
 	copts.CommitFromIncl = s.opts.CommitFromIncl
 	copts.AllBranches = s.opts.AllBranches
-	copts.Logger = s.opts.Logger
 	cm := commitmeta.New(s.opts.RepoDir, copts)
 	res, err := cm.RunMap()
 	if err != nil {
